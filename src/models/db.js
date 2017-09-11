b090447f2381eb7f79eeefa26fd943cd03103efa
vol4tim <vol4tim@gmail.com>
2017-09-11 16:59:38 +0400

    remove db

--- src/models/db.js
+++ src/models/db.js
@@ -3,7 +3,7 @@ import Sequelize from 'sequelize'
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: path.join(__dirname, '/../../database.sqlite'),
+  storage: path.join(__dirname, '/../../.../database.sqlite'),
   logging: false
 });
 
